95c42ea11c4b05ca26333bb7a6c31631902ec605
md-azmi-siddique <md.azmi.siddique@gmail.com>
2024-05-27 23:03:31 +0600

    master layout

--- front_end/src/components/MasterLayout.jsx
+++ front_end/src/components/MasterLayout.jsx
@@ -1,7 +1,6 @@
 // import React from 'react';
-
-import AppBar from "./AppBar";
-import Footer from "./Footer";
+import AppBar from './AppBar';
+import Footer from './Footer';
 
 const MasterLayout = (props) => {
     return (
